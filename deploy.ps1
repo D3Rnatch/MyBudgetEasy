43c994f0c65a993425556cf054fe669aeb12b596
D3Rnatch <dernatch@gmail.com>
2024-03-02 13:15:10 +0100

    Updated deployment script.

--- deploy.ps1
+++ deploy.ps1
@@ -5,3 +5,9 @@ firebase emulators:start
 
 # To deploy run this:
 firebase deploy --only hosting
+
+# Deploy on server:
+# firebase deploy --only hosting:mywallet-dc4cd
+# But first create a firebase and register the app, then modify the firebase.json file.
+# then build: npm run build
+# Finally deploy to host. Website will be available soon enough!
